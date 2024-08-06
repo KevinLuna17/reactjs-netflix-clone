6911ba1801c181c668c1b47123bfba6874b54361
KevinLuna17 <kelugo_14@hotmail.com>
2024-08-06 18:08:45 -0500

    Change Navbar list items to spanish

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
       </div>
       <div className='navbar-right'>
         <img src={search_icon} alt='search_icon' className='icons' />
-        <p>Children</p>
+        <p>Fititos</p>
         <img src={bell_icon} alt='bell_icon' className='icons' />
         <div className='navbar-profile'>
           <img src={profile_img} alt='profile_img' className='profile' />
