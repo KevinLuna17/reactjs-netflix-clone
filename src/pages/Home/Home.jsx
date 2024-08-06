5427b890bc09cc1cac1114441e77ad87bd0e9e9f
KevinLuna17 <kelugo_14@hotmail.com>
2024-08-06 17:39:48 -0500

    Mount Footer component to Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
 import TitleCards from '../../components/TitleCards/TitleCards'
+import Footer from '../../components/Footer/Footer'
 import hero_banner from '../../assets/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
 import play_icon from '../../assets/play_icon.png'
@@ -32,6 +33,7 @@ const Home = () => {
         <TitleCards title={"Continuar viendo"}/>
         <TitleCards title={"Lo nuevo en Netflix"}/>
       </div>
+      <Footer/>
     </div>
   )
 }
