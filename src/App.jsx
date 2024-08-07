c1910fd8e99bb6444cc4e0f7149f473121578fbb
KevinLuna17 <kelugo_14@hotmail.com>
2024-08-07 18:21:18 -0500

    Add react-router-dom and create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Home from './pages/Home/Home'
+import Player from './pages/Player/Player'
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-      <Home/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/player' element={<Player/>}/>
+      </Routes>
     </div>
   )
 }
